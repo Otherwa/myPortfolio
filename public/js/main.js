20ae83b18a1e01e384e727068eead37e01d0cbc6
Otherwa <67428572+Atharv2002@users.noreply.github.com>
2023-08-26 17:58:11 +0530

    /changes/

--- public/js/main.js
+++ public/js/main.js
@@ -3,7 +3,9 @@ import { } from "./splash.js";
 import { } from "./dark.js";
 import { sr } from './slider.js'
 import { literallyme } from "./me.js";
-// swup
+// ? swup
+sr.reveal('#main', { delay: 200, origin: "left" })
+
 const swup = new Swup({
     containers: ["#swup"]
 });
@@ -11,13 +13,16 @@ const swup = new Swup({
 console.log("%cdesign & coded by Atharv Desai", "color: yellow; background-color: blue;padding: 2px");
 console.log(literallyme);
 
+
 swup.hooks.on('page:view', () => {
-    // This runs after every page change by swup
+    // ? This runs after every page change by swup
     sr.reveal('.about-left', { delay: 200, origin: "left" })
+    sr.reveal('#main', { delay: 200, origin: "left" })
     sr.reveal('.about-right', { delay: 200, origin: "top" })
     sr.reveal('.wrapper', { delay: 100, origin: "bottom" })
     sr.reveal('.wrapper1', { delay: 100, origin: "top" })
-    sr.reveal('#prj', { delay: 150, origin: "top" })
+    sr.reveal('.wrapper2', { delay: 100, origin: "right" })
+    sr.reveal('#prj li', { delay: 150, origin: "top" })
 });
 
 
